f8be8ed9f7b45de0ebb302cd7a27f23d875ac501
aisanashukirbekova <aisanashukirbekova@gmail.com>
2025-03-27 22:40:21 +0500

    Заливаем файлы

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('.button').addEventListener('click', function() {
+        alert('Вы нажали на кнопку!');
+    });
+});
+
